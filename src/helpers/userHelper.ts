1e277285cebaac896db53cd99d436f5a27d63b2e
Сорок два <sorokdva.developer@gmail.com>
2022-04-04 02:13:34 +0200

    wip(feat): unable users to change their names

--- src/helpers/userHelper.ts
+++ src/helpers/userHelper.ts
@@ -1,12 +1,29 @@
 import { Client, GuildMember, PartialGuildMember, Role } from 'discord.js'
 
+// @ts-ignore
+const history: [{
+  identifier: string,
+  nickname: string,
+}] = []
+
 const handleChange = async (client: Client, user: GuildMember | PartialGuildMember, newMember: GuildMember): Promise<void> => {
+  console.log(history)
   const blockedNicknameRole = <Role>user.guild?.roles.cache.get('960289996382797854')
-  
-  if (user.roles.cache.has(blockedNicknameRole?.id)) {
-    console.log('cant change nickname', 'changing to', user.user?.username)
-    // @ts-ignore
-    user.guild.members.cache.get(user.id).setNickname(user.user?.username ?? 'Pseudo à changer')
+  if (user && newMember && user.roles.cache.has(blockedNicknameRole?.id)) {
+    if (history.length > 0 && history[history.length - 1].identifier === <string>user.user?.username) {
+      if (history[history.length - 1].nickname === newMember.nickname) return
+    }
+    if (user.nickname !== newMember.nickname) {
+      const currentUser = user.guild.members.cache.get(user.id)
+      // @ts-ignore
+      console.log('cant change nickname', newMember.nickname, 'changing to', user.nickname)
+      history.push({
+        identifier: <string>user.user?.username,
+        nickname: <string>user.nickname,
+      })
+      // @ts-ignore
+      currentUser.setNickname(user.nickname ?? user?.user.username)
+    }
   }
 }
 
